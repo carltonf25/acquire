c38a5d354e03c54b8aa57bd887e50e36bc71353a
Carlton Freeman <carltonfreeman@carltons-mbp.lan>
2020-02-06 00:46:06 -0500

    implement icons in nav (just pngs for now, may fiddle with svg animation if there's time at the end)

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
+import homeIcon from '../../img/home-icon.png';
+import targetIcon from '../../img/target-icon.png';
+import createIcon from '../../img/create-icon.png';
+
 export const Nav = () => {
   return (
     <nav className="side-nav">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img alt="home" src={homeIcon} />
+          </Link>
         </li>
         <li>
-          <Link to="/targets">Targets</Link>
+          <Link to="/targets">
+            <img alt="targets" src={targetIcon} />
+          </Link>
         </li>
         <li>
-          <Link to="/create">Create Target</Link>
+          <Link to="/create">
+            <img alt="create-target" src={createIcon} />
+          </Link>
         </li>
       </ul>
     </nav>
--- src/components/TargetDetails/InfoCard.js
+++ src/components/TargetDetails/InfoCard.js
@@ -4,9 +4,9 @@ import StatusBadge from '../StatusBadge';
 const InfoCard = ({ name, status, contact }) => {
   return (
     <div className="card metric-card company-info-card">
-      <h4>{name}</h4>
+      <h3>{name}</h3>
       <p>Status: {status}</p>
-      <p>Contact:{contact}</p>
+      <p>Contact: {contact}</p>
     </div>
   );
 };
