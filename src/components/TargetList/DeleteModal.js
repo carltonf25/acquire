2bca963dacbce1c09c12bc260086b80a7d3c1350
carltonf25 <carltonfreeman24@gmailcom>
2020-02-08 14:59:08 -0500

    modal button refinement

--- src/components/TargetList/DeleteModal.js
+++ src/components/TargetList/DeleteModal.js
@@ -12,7 +12,7 @@ const DeleteModal = ({ deleteTarget, target, setModalOpen }) => {
         <div className="overlay">
             <animated.div style={slideDown} className="modal delete-confirm">
                 <h2>Are you sure you want to delete {target.name}?</h2>
-                <button className="btn primary" onClick={() => deleteTarget(target.id)}>Yes, I'm sure</button>
+                <button className="btn primary delete" onClick={() => deleteTarget(target.id)}>Yes, I'm sure</button>
                 <button className="btn secondary" onClick={() => setModalOpen(false)}>Nevermind!</button>
             </animated.div>
         </div>
